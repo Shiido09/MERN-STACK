4514f8161f94d41597e0d858aefb7cdf5a690311
Joshie <joshziemenn.tan@tup.edu.ph>
2024-11-19 21:40:41 +0800

    Update product.route.js

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,24 +1,14 @@
-// routes/product.route.js
 import express from "express";
-import { createProduct, getAllProducts, getProductById, updateProduct, deleteProduct } from "../controllers/product.controller.js";
-import multer from "multer";
+import { createProduct, updateProduct, deleteProduct, getAllProducts, getProductById } from "../controllers/product.controller.js";
+import upload from "../utils/multer.js";
 import { isAuthenticatedUser, authorizeAdmin } from "../middleware/authUsers.js";
+
 const router = express.Router();
-const upload = multer({ dest: "uploads/" }); // Adjust if you have a specific multer config
 
-// Create a product (can be done without images)
-router.post("/", isAuthenticatedUser, authorizeAdmin,upload.array("product_images", 5), createProduct);
-
-// Get all products
-router.get("/",isAuthenticatedUser, authorizeAdmin ,getAllProducts);
-
-// Get a product by ID
-router.get("/:id",isAuthenticatedUser, authorizeAdmin ,getProductById);
-
-// Update a product by ID (can be done with images)
-router.put("/:id",isAuthenticatedUser, authorizeAdmin, upload.array("product_images", 5), updateProduct);
-
-// Delete a product by ID
-router.delete("/:id",isAuthenticatedUser, authorizeAdmin,deleteProduct);
+router.post("/", isAuthenticatedUser, authorizeAdmin, upload.array("product_images", 5), createProduct);
+router.put("/:id", isAuthenticatedUser, authorizeAdmin, upload.array("product_images", 5), updateProduct);
+router.delete("/:id", isAuthenticatedUser, authorizeAdmin, deleteProduct);
+router.get("/", isAuthenticatedUser, authorizeAdmin, getAllProducts);
+router.get("/:id", isAuthenticatedUser, authorizeAdmin, getProductById);
 
 export default router;
