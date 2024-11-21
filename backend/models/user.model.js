a491fc1ae2099ba7a0429b83e2907c47dd5f34eb
Joshie <joshziemenn.tan@tup.edu.ph>
2024-11-21 19:01:56 +0800

    Update user.model.js

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,39 +1,56 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: true,
-        unique: true,
+const userSchema = new mongoose.Schema(
+    {
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        name: {
+            type: String,
+            required: true,
+        },
+        lastLogin: {
+            type: Date,
+            default: Date.now,
+        },
+        isVerified: {
+            type: Boolean,
+            default: false,
+        },
+        isAdmin: {
+            type: Boolean,
+            default: false,
+        },
+        resetPasswordToken: String,
+        resetPasswordExpiresAt: Date,
+        verificationToken: String,
+        verificationTokenExpiresAt: Date,
+        firebaseUid: {
+            type: String,
+            required: true,
+        },
+        cart: [
+            {
+                product: {
+                    type: mongoose.Schema.Types.ObjectId, // Reference to a product model
+                    ref: "Product",
+                    required: true,
+                },
+                quantity: {
+                    type: Number,
+                    required: true,
+                    min: 1,
+                },
+            },
+        ],
     },
-    password: {
-        type: String,
-        required: true,
-    },
-    name: {
-        type: String,
-        required: true,
-    },
-    lastLogin: { 
-        type: Date,
-        default: Date.now,
-    },
-    isVerified: {
-        type: Boolean,
-        default: false,
-    },
-    isAdmin: {
-        type: Boolean,
-        default: false,
-    },
-    resetPasswordToken: String,
-    resetPasswordExpiresAt: Date,
-    verificationToken: String,
-    verificationTokenExpiresAt: Date,
-    firebaseUid: {
-        type: String,
-        required: true,
-    },
-}, { timestamps: true });
+    { timestamps: true }
+);
 
 export const User = mongoose.model("User", userSchema);
